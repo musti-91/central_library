172ce14cdb15657fbc314035112e96c2e3f907a3
Mustafa <mustafa.alroomi.91@gmail.com>
2025-01-11 01:35:33 +0100

    login & move into pages

---
+++ js/login.js
@@ -0,0 +1,86 @@
+const USER_INFO_KEY = "user_info"
+
+// get elements
+const email = document.getElementById("input_email")
+const password = document.getElementById("input_password")
+const confirmationPassword = document.getElementById("input_password_confirmation")
+const selectRole = document.getElementById("select_role")
+
+const errorEmail = document.getElementById("error_email")
+const errorPassword = document.getElementById("error_password")
+const errorConfirmPassword = document.getElementById("error_password_confirmation")
+const errorRole = document.getElementById("error_role")
+
+const form = document.getElementById("register_form")
+
+// add event listener
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  let errors = false
+  // check email
+  if (email.value === "") {
+    errorEmail.innerHTML = "Email is required"
+    errors = true
+  } else if (email.value.indexOf("@") === -1) {
+    errorEmail.innerHTML = "Email must contain @"
+    // TODO: To change the validation rule.
+  } else if (email.value.indexOf("uk.com") === -1) {
+    errorEmail.innerHTML = "Not allowed"
+  } else {
+    errorEmail.innerHTML = ""
+  }
+
+  // check password
+  if (password.value === "") {
+    errorPassword.innerHTML = "Password is required"
+    errors = true
+  } else if (password.value.length < 8) {
+    errorPassword.innerHTML = "Password must be at least 8 characters"
+    errors = true
+  } else {
+    errorPassword.innerHTML = ""
+  }
+
+  // check confirmation password
+  if (confirmationPassword.value === "") {
+    errorConfirmPassword.innerHTML = "Confirmation password is required"
+    errors = true
+  } else {
+    errorConfirmPassword.innerHTML = ""
+  }
+
+  // check role
+  if (selectRole.value === "") {
+    errorRole.innerHTML = "Role is required"
+    errors = true
+  } else {
+    errorRole.innerHTML = ""
+  }
+
+  if (!errors) {
+    if (password.value !== confirmationPassword.value) {
+      errorConfirmPassword.innerHTML = "Password and confirmation password must be the same"
+      e.preventDefault()
+    } else {
+      errorConfirmPassword.innerHTML = ""
+      login(email.value, password.value, selectRole.value)
+    }
+  }
+})
+
+function login(email, password, role) {
+  // clear the fields
+  clearFields()
+
+  // set user info in local storage
+  localStorage.setItem(USER_INFO_KEY, JSON.stringify({ email, role }))
+  // redirect to dashboard
+  window.location.href = "../index.html"
+}
+
+function clearFields() {
+  email.value = ""
+  password.value = ""
+  confirmationPassword.value = ""
+  selectRole.value = ""
+}
